318a5847ae9407a5696a030579e20580b06b7381
javigarciant <jantgarcia@falabella.cl>
2020-08-02 16:52:37 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require('./config/config')
+const express = require('express');
+const { response } = require('express');
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port`);
+});
+
+app.get('/', (req, res) => {
+    res.json('Hello-GCP');
+});
+
+app.get('/adios', (req, res) => {
+    res.json('Bye-GCP');
+});
